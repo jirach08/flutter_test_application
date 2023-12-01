b61c5fba2ed3e2aabca4bc0a7c5a31a722479ed2
jirach08 <lawrence.smith@wwu.de>
2023-12-01 12:43:45 +0100

    Make notifier list dynamic clean up notifier constructors

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (context) => MyRoomStatesNotifier(),
+      create: (context) => MyRoomStatesNotifier(10),
       child: MaterialApp(
         title: 'Meetly',
         theme: ThemeData(
@@ -26,30 +26,24 @@ class MyApp extends StatelessWidget {
 
 class MyRoomState{
   int id = 0;
-  String name = "room0";
+  String name = "";
   bool occupied = false;
 
-  MyRoomState(int id, String name){
-    this.id = id;
-    this.name = name;
-    this.occupied = false;
-  }
+  MyRoomState(this.id, this.name){ occupied = false; }
 
-  void toggle(){
-    occupied = !occupied;
-  }
-  Color get color {
-    if (occupied) {
-      return Colors.red;
-    }
-    else {
-      return Colors.green;
-    }
-  }
+  void toggle() { occupied = !occupied; }
 }
 
 class MyRoomStatesNotifier extends ChangeNotifier {
-  var rooms = [for (int i in [0,1,2]) MyRoomState(i, "Raum $i")];
+  MyRoomStatesNotifier(this.nRooms){
+    for (int i = 0; i < nRooms; i++) {
+      rooms.add(MyRoomState(i, "Raum $i"));
+    }  
+  }
+
+  int nRooms = 0;
+  List<MyRoomState> rooms = [];
+  
   void toggle(int i) {
     rooms[i].toggle();
     notifyListeners();
@@ -66,14 +60,14 @@ class MyHomePage extends StatelessWidget {
         children: [
           for (var room in appState.rooms)
             ElevatedButton(
-              child: Text(room.name),
               onPressed: (){
                 appState.toggle(room.id);
               },
               style: ButtonStyle(
-                backgroundColor:  MaterialStateProperty.all<Color>(room.color),
+                backgroundColor:  MaterialStateProperty.all<Color>(room.occupied ? Colors.deepOrange: Colors.green),
                 foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
               ),
+              child: Text(room.name),
             ),
           ],
         ),
